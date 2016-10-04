d69c163eb97b17d9a2bd3faec094993f2415cdf7
Marioisdabest <mtutuncumacias1@gmail.com>
2016-10-04 12:01:14 -0400

    Create cardmessages.js

---
+++ cardmessages.js
@@ -0,0 +1,16 @@
+let cardmessages=["This is a SHOWBIZ card. \n Find a pencil, stick in between your mouth and nose, and pretend to groom your moustache.","This is a QUIZZLE card. \n What came first: \n A. The chicken \n B. The egg","This is a ROOLZ card. \n This is a Talking Rule. Every time you take a sip of water, you must say, 'I need my pills!'","This is a STUNTZ card. \n You are now the mummy. Get some toilet paper and mummify your face. Keep your wrappings on until any player rolls a four.", "This is a SCATTERBRAINZ card. \n Choose a topic: 'Types of nuts' or 'Colors of crayons'.", "This is a SHOWBIZ card. \n Talk in a Transylvanian accent and tell a fellow player, 'I vant to suck your blood.'", "This is a QUIZZLE card. \n Where did the tallest person in the world live? \n A. The United States \n B. Turkey \n C. Bulgaria \n D. Ethiopia", "This is a ROOLZ card. \n This is an Action Rule. For the rest of the game, you must stand, unless it is your turn.","This is a STUNTZ card. \n Stand on one foot and hum the national anthem until any player rolls a six.","This is a SCATTERBRAINZ card. \n Choose a topic: 'Ways to fry eggs' or 'Embarassing situations'.","This is a SHOWBIZ card. \n In a completely calm and collected tone, explain how you had a delicious child for breakfast yesterday.","This is a QUIZZLE card. \n What color is complementary of blue on the color wheel? \n A. Red \n B. Purple \n C. Orange \n D. Yellow","This is a ROOLZ card. \n This is a Global Rule. Every time you roll the die, you must say 'I once consumed a delicious child!'","This is a STUNTZ card. \n Construct a fort made out of pillows. Stay in the fort until your next turn.","This is a SCATTERBRAINZ card. \n Choose a topic: 'Names of a rivers that an aircraft carrier could not float down' or 'Sounds people often make that don't contain words'.","This is a SHOWBIZ card. \n Pick up a newspaper and attempt to paper-machê your feet.","This is a QUIZZLE card. \n What is the capital of Ireland? \n A. Dublin \n B. Amman \n C. Belfast \n D. Galway","This is a ROOLZ card. \n This is a Talking Rule. Every time an opponent passes you on the playing board, you must say, 'You've thwarted us, you vile beast!'","This is a STUNTZ card. \n Wear a sock as a tie for five minutes.","This is a SCATTERBRAINZ card. \n Choose a topic: 'Things you can see your reflection in' or 'Excuses you've used to avoid chores'."]
+let cardanswers=["","b","","","","","b","","","","","c","","","","","a","","",""]
+let rules=[7,7,0,4,7,7,7,10,6,7,7,7,8,9,7,7,7,0,11,7]
+console.log(rules.length)
+/*
+All rules have a code:
+
+0 means a personal talking rule
+Anything 1-6 means that this rule ends when that number is rolled
+7 means a one time rule
+8 means a global rule
+9 means an until your next turn rule
+10 means a personal action rule
+11 means for five minutes
+
+*/
